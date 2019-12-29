0798a345180307e6f0f7cf9b69060d12ceb75aaa
fanjieqi <fjqqiqi@163.com>
2019-12-29 23:38:03 +0800

    Add 102. Binary Tree Level Order Traversal

---
+++ 101-200/102. Binary Tree Level Order Traversal.rb
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+def dfs(result, root, n)
+  return if root.nil?
+  result[n] ||= []
+  result[n] << root.val
+  dfs(result, root.left, n+1)
+  dfs(result, root.right, n+1)
+end
+
+# @param {TreeNode} root
+# @return {Integer[][]}
+def level_order(root)
+  result = []
+  dfs(result, root, 0)
+  result
+end
