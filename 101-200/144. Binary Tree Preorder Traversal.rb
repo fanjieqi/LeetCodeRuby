15dea02b9f4733db450e391ebaf9b0c3b2dd83f1
fanjieqi <fjqqiqi@163.com>
2019-12-31 13:40:21 +0800

    Create 144. Binary Tree Preorder Traversal.rb

---
+++ 101-200/144. Binary Tree Preorder Traversal.rb
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer[]}
+def preorder_traversal(root)
+  return [] if root.nil?
+  [root.val] + preorder_traversal(root.left) + preorder_traversal(root.right)
+end
