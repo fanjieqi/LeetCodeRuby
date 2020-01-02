8439e7b86494dc81e93d9db4d3718b0ae1fd4984
fanjieqi <fjqqiqi@163.com>
2020-01-02 15:18:06 +0800

    Create 237. Delete Node in a Linked List.rb

---
+++ 201-300/237. Delete Node in a Linked List.rb
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} node
+# @return {Void} Do not return anything, modify node in-place instead.
+def delete_node(node)
+  while node.next && node.next.next
+    node.val = node.next.val
+    node = node.next
+  end
+  node.val = node.next.val
+  node.next = nil
+end
