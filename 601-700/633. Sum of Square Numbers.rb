f87ea5aa72df1249dd840fa906a879ce6406e565
fanjieqi <fjqqiqi@163.com>
2020-03-07 20:33:31 +0800

    Update 633. Sum of Square Numbers.rb

--- 601-700/633. Sum of Square Numbers.rb
+++ 601-700/633. Sum of Square Numbers.rb
@@ -1,6 +1,5 @@
 # @param {Integer} c
 # @return {Boolean}
 def judge_square_sum(c)
-  k = Math.sqrt(c)
-  (0..k).any? {|i| Math.sqrt(c - (i**2)).to_i**2 == c - (i**2) } 
+  (0..Math.sqrt(c)).any? {|i| Math.sqrt(c - (i**2)).to_i**2 == c - (i**2) } 
 end
