c5b84164aeb7efe9ca56b454a6ae6f3654e7db23
fanjieqi <fjqqiqi@163.com>
2020-04-18 18:17:18 +0800

    Create 814. Binary Tree Pruning.rb

---
+++ 801-900/814. Binary Tree Pruning.rb
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {TreeNode}
+def prune_tree(root)
+  return nil if root.nil?
+  root.left = prune_tree(root.left)
+  root.right = prune_tree(root.right)
+  root.left.nil? && root.right.nil? && root.val==0 ? nil : root
+end
