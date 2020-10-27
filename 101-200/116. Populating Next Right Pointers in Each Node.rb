e5957e8082b89b9824e80db4ec0330d42c79cc0f
fanjieqi <fjqqiqi@163.com>
2020-10-27 21:59:22 +0800

    Create 116. Populating Next Right Pointers in Each Node.rb

---
+++ 101-200/116. Populating Next Right Pointers in Each Node.rb
@@ -0,0 +1,24 @@
+# Definition for Node.
+# class Node
+#     attr_accessor :val, :left, :right, :next
+#     def initialize(val)
+#         @val = val
+#         @left, @right, @next = nil, nil, nil
+#     end
+# end
+
+def preorder(node, level = 0)
+  return if node.nil?
+  @prev[level].next = node if @prev[level]
+  @prev[level] = node
+  preorder(node.left, level + 1)
+  preorder(node.right, level + 1)
+end
+
+# @param {Node} root
+# @return {Node}
+def connect(root)
+  @prev = []
+  preorder(root)
+  root
+end
